c5b187ecd99a58d4f51f34f793edecdb4fd91a89
ibkxyz <ibkxyz@gmail.com>
2017-04-06 10:24:11 +0100

    Added another activity, still working on the login

---
+++ app/src/main/java/com/ibkxyz/anonymous/darkmobile/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.ibkxyz.anonymous.darkmobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.R.id.message;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+
+           }
+}
--- app/src/main/java/com/ibkxyz/anonymous/darkmobile/MainActivity.java
+++ app/src/main/java/com/ibkxyz/anonymous/darkmobile/MainActivity.java
@@ -1,7 +1,13 @@
 package com.ibkxyz.anonymous.darkmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.R.id.message;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /** Called when the user taps the Go button */
+    public void login(View view) {
+        // Do something in response to button Go
+
+//        Intent intent = new Intent(this, Main2Activity.class);
+        EditText passCode = (EditText) findViewById(R.id.editTextLogin);
+        if (passCode.getText().equals("1010")){
+
+
+        }
+//        intent.putExtra(EXTRA_MESSAGE, passCode);
+//        startActivity(intent);
+
+
+
+
     }
 }
